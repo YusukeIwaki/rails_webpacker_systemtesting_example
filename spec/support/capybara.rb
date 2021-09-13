b8c32055101a7ea4dd3e05d95155facf452f9535
YusukeIwaki <iwaki@i3-systems.com>
2021-09-13 15:05:47 +0900

    add trace

--- spec/support/capybara.rb
+++ spec/support/capybara.rb
@@ -1,3 +1,17 @@
 require 'capybara'
 
 Capybara.default_max_wait_time = 15
+
+def trace(klass)
+  klass.public_instance_methods(false).each do |method_sym|
+    orig = klass.instance_method(method_sym)
+    klass.define_method(method_sym) do |*args, **kwargs, &block|
+      puts "START: #{klass.name}##{method_sym} #{args.map(&:to_s)} #{kwargs.map {|k, v| "#{k.to_s} => #{v.to_s}"}}"
+      orig.bind(self).call(*args, **kwargs, &block)
+    end
+  end
+end
+trace(Capybara::Selenium::Driver)
+trace(Capybara::Selenium::Node)
+trace(Capybara::Selenium::Find)
+trace(Capybara::DSL)
