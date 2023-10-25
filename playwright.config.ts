c8ef6234b15ea360e4303fb8113386f1f0052d13
YusukeIwaki <iwaki@i3-systems.com>
2023-10-26 00:07:23 +0900

    ワーカー数を1に

--- playwright.config.ts
+++ playwright.config.ts
@@ -16,9 +16,9 @@ export default defineConfig({
   /* Fail the build on CI if you accidentally left test.only in the source code. */
   forbidOnly: !!process.env.CI,
   /* Retry on CI only */
-  retries: process.env.CI ? 2 : 0,
+  retries: 2,
   /* Opt out of parallel tests on CI. */
-  workers: process.env.CI ? 1 : undefined,
+  workers: 1,
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
   reporter: 'html',
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
@@ -72,6 +72,6 @@ export default defineConfig({
   webServer: {
     command: 'RAILS_ENV=test bundle exec rails s',
     url: 'http://127.0.0.1:3000',
-    reuseExistingServer: !process.env.CI,
+    reuseExistingServer: false,
   },
 });
